61609ff72f8e17f46ffec2a5db60ca9a9658dd24
Jesse McKenzie <jfmckenz31@gmail.com>
2018-07-14 16:00:38 -0400

    fixed navbar to sticky, deleted position: fixed; in .sticky class

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 
-//  function showNav() {
-//      var blah = document.getElementById("work");
-//      if (blah.style.display === none) {
-//          blah.style.display = "block";
-//      } else {.
-//          blah.style.display = "none";
-//      }
-//  }
 
 
 $("button.navButton").click(function()    {
