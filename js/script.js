0a93521e9609466206aea0d58744f1097bd7b02b
Jesse McKenzie <jfmckenz31@gmail.com>
2018-07-19 22:47:03 -0400

    added js description to script.js functions

--- js/script.js
+++ js/script.js
@@ -1,40 +1,22 @@
 
 // Jquery tells browser to listen for a click event
 $("button.burger").click(function () {  // when button.burger is activated run function:
-    $(".items").toggle(); // toggle class .itmes on click
+    $(".items").toggle(); // toggle class .itmes on click event
 });
 
 
 
-window.onscroll = function () {  //tells browser to listen to scroll and be ready for a function
+window.onscroll = function () {  //tells browser to listen for a verticle scroll event and be ready to execute a function
     myFunction();
 };
 
-var header = document.getElementById("naviBar");  //
-var hold = header.offsetTop;
+var header = document.getElementById("naviBar");  //target class naviBar and declare as variable header
+var hold = header.offsetTop;  //offset height of the header element is declared as var hold 
 
 function myFunction() {
-    if (window.pageYOffset > hold) {    //class hold is a child element of pageYoffset
-        header.classList.add("hold");  //class hold is added to the header div
+    if (window.scrollY > hold) {    //If the window scrolls verically, activate class hold
+        header.classList.add("hold");  //class hold is added to the header section, sticky effect is activated
     } else {
-        header.classList.remove("hold");  //class hold is removed from the header div
+        header.classList.remove("hold");  //class hold is removed from the header section if the broswer does not scroll or the scroll exceeds the header verticle height
     }
 }
-
-
-
-// $(".anthem").hover(function () {
-//     $(this).toggleClass("hovered");
-//     $(".anthemText").toggleClass("hovered");
-// });
-
-// $(".anthem").click(function () {
-//     $(this).toggleClass("hovered");
-//     $(".anthemText").toggleClass("hovered");
-// });
-
-$(document).ready(function () {
-    $("question").click(function () {
-        $(this).show("answer");
-    });
-});
