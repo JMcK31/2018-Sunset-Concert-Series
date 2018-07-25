89f83374905d9e25c764fb4ac73fd05c692d40cb
Jesse McKenzie <jfmckenz31@gmail.com>
2018-07-25 14:10:08 -0400

    test branch push

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 
 // Jquery tells browser to listen for a click event
-$("button.burger").click(function () {  // when button.burger is activated run function:
-    $(".items").toggle(); // toggle class .itmes on click event, toggle activates .items display: block;
+$("button.burger").click(function () {  // when button.burger is activated run function:  click is button handler" click event handler added to "button.burger"
+    $(".items").toggle(); //  click event activates turns on .items display: block;
 });
 
 //chain a function to turn off navBar row to setup slideToggle function above 
 
-window.onscroll = function () {  //tells browser to listen for a verticle scroll event and be ready to execute a function
+window.onscroll = function () {  //tells browser to listen for a verticle scroll event handler and be ready to execute a function
     myFunction();
 };
 
-var header = document.getElementById("naviBar");  //target class naviBar and declare as variable header
-var hold = header.offsetTop;  //offset height of the header element is declared as var hold 
+var header = document.getElementById("naviBar");  //targets class naviBar and di]eclare as variable header
+var hold = header.offsetTop;  //offset height of the header variable is declared as var hold 
 
 function myFunction() {
-    if (window.scrollY > hold) {    //If the window scrolls verically, activate class hold
+    if (window.scrollY > hold) {    //mouse event verticle scroll is a parent to class hold. 
         header.classList.add("hold");  //class hold is added to the header section, sticky effect is activated
     } else {
         header.classList.remove("hold");  //class hold is removed from the header section if the broswer does not scroll or the scroll exceeds the header verticle height
