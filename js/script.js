42920461611ec19735b5ec3daff1fb617f9ca3f0
Jesse McKenzie <jfmckenz31@gmail.com>
2018-07-25 16:50:45 -0400

    added questions to about2 page

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 // Jquery tells browser to listen for a click event
-$("button.burger").click(function () { // when button.burger is activated run function:  click is button handler" click event handler added to "button.burger"
-    $(".items").toggle(); //  click event activates turns on .items display: block;
+$("button.burger").click(function () { // click event handler tells browser to "do something" when button.burger engaged 
+    $(".items").toggle(); //  click evecnt activates toggle function, .items display: block is activated on toggle click and deactivated on toggle click
 });
-//chain a function to turn off navBar row to setup slideToggle function above 
+
+
+//js for navigation bar stick to header, until reaching bottom of header
 window.onscroll = function () { //tells browser to listen for a verticle scroll event handler and be ready to execute a function
     myFunction();
 };
-var header = document.getElementById("naviBar"); //targets class naviBar and di]eclare as variable header
+var header = document.getElementById("naviBar"); //targets class naviBar and declares as variable header
 var hold = header.offsetTop; //offset height of the header variable is declared as var hold 
 function myFunction() {
     if (window.scrollY > hold) { //mouse event verticle scroll is a parent to class hold. 
         header.classList.add("hold"); //class hold is added to the header section, sticky effect is activated
     } else {
-        header.classList.remove("hold"); //class hold is removed from the header section if the broswer does not scroll or the scroll exceeds the header verticle height
+        header.classList.remove("hold"); //class hold is removed from the header section if the browser does not scroll or the scroll exceeds the header verticle height
     }
 }
+
+
 //about page q&a
 $(document).ready(function () {
     $('.qAnda li p').hide();
